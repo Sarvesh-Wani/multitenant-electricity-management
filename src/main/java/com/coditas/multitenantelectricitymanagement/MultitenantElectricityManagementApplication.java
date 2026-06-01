69832d63e868d48c37881b7856094556d89f629f
Sarvesh Wani <sarvesh.wani@coditas.com>
2026-06-01 11:13:57 +0530

    Initial commit

---
+++ src/main/java/com/coditas/multitenantelectricitymanagement/MultitenantElectricityManagementApplication.java
@@ -0,0 +1,13 @@
+package com.coditas.multitenantelectricitymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitenantElectricityManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultitenantElectricityManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coditas/multitenantelectricitymanagement/MultitenantElectricityManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coditas.multitenantelectricitymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitenantElectricityManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
